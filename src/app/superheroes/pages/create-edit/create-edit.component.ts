8def48f0a3f5bc7eeeae2bbf8e727a272b66513f
Will Cumbicus <wilmanvc93@gmail.com>
2024-05-31 23:47:27 +0200

    feat: create form

--- src/app/superheroes/pages/create-edit/create-edit.component.ts
+++ src/app/superheroes/pages/create-edit/create-edit.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-create-edit',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormField,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   templateUrl: './create-edit.component.html',
-  styleUrl: './create-edit.component.scss'
+  styleUrl: './create-edit.component.scss',
 })
 export class CreateEditComponent {
+  form = new FormGroup({
+    name: new FormControl(''),
+    job: new FormControl(''),
+  });
 
+  protected onSubmit() {
+    console.log(this.form.value);
+  }
 }
