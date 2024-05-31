38839ea8e038335ce7d75857ec26ba94f7219eae
Will Cumbicus <wilmanvc93@gmail.com>
2024-05-31 18:10:21 +0200

    refactor: create goEdit and goCreate

--- src/app/superheroes/pages/list/list.component.ts
+++ src/app/superheroes/pages/list/list.component.ts
@@ -5,6 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { SuperheroesService } from '../../../services/superheroes.service';
 @Component({
   selector: 'app-list',
@@ -17,12 +18,16 @@ import { SuperheroesService } from '../../../services/superheroes.service';
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
+    RouterLink,
   ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
   private readonly superheroesService = inject(SuperheroesService);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
+
   protected users$ = computed(() => this.superheroesService.getUsers());
 
   protected user$ = computed(() => this.superheroesService.getUser());
@@ -32,4 +37,16 @@ export class ListComponent {
   protected modifyUser$ = computed(() => this.superheroesService.modifyUser());
 
   protected deleteUser$ = computed(() => this.superheroesService.deleteUser());
+
+  protected goEdit(id: number): void {
+    this.router.navigate(['../edit', id], {
+      relativeTo: this.route,
+    });
+  }
+
+  protected goCreate(): void {
+    this.router.navigate(['../create'], {
+      relativeTo: this.route,
+    });
+  }
 }
