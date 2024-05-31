64b59be35a34f70441d1a2343764178bd07bcc50
Will Cumbicus <wilmanvc93@gmail.com>
2024-05-31 17:54:01 +0200

    feat: add search

--- src/app/superheroes/pages/list/list.component.ts
+++ src/app/superheroes/pages/list/list.component.ts
@@ -2,12 +2,22 @@ import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, computed, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { SuperheroesService } from '../../../services/superheroes.service';
-
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [JsonPipe, AsyncPipe, MatCardModule, MatButtonModule],
+  imports: [
+    JsonPipe,
+    AsyncPipe,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
